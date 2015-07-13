aac4987dc777ede47cd83457567e5ef458215657
Lijy1498 <liam.girard@gmail.com>
2015-07-13 17:20:20 -0400

    Snapping to Grid

--- js/MoreHelpPlz.js
+++ js/MoreHelpPlz.js
@@ -43,12 +43,12 @@ function handleMove(evt) {
   //for each touch
   for (var i = 0; i < evt.changedTouches.length; i++)  {	
     //the selected tiles
-    if (selectedTile >= 0) {
+    if (selectedTile[i] >= 0) {
       //move selected tiles
       board.tile[selectedTile[i]].oldX = board.tile[selectedTile[i]].X
       board.tile[selectedTile[i]].oldY = board.tile[selectedTile[i]].Y
-      board.tile[selectedTile[i]].X = evt.changedTouches[i].pageX - board.tile[selectedTile].distX;
-      board.tile[selectedTile[i]].Y = evt.changedTouches[i].pageY - board.tile[selectedTile].distY;
+      board.tile[selectedTile[i]].X = evt.changedTouches[i].pageX - board.tile[selectedTile[i]].distX;
+      board.tile[selectedTile[i]].Y = evt.changedTouches[i].pageY - board.tile[selectedTile[i]].distY;
       //redraw tiles
       draw();
     }
@@ -61,8 +61,15 @@ function handleEnd(evt) {
   
   //for each touch
   for (var i = 0; i < evt.changedTouches.length; i++)  {  
-    //no longer select the tile
-    selectedTile[i] = -1;
+    if (selectedTile[i] >= 0) {
+      board.tile[selectedTile[i]].oldX = board.tile[selectedTile[i]].X;
+      board.tile[selectedTile[i]].oldY = board.tile[selectedTile[i]].Y;
+      board.tile[selectedTile[i]].X = (Math.floor((board.tile[selectedTile[i]].X+30)/60)*60)+5
+      board.tile[selectedTile[i]].Y = (Math.floor((board.tile[selectedTile[i]].Y+30)/60)*60)+5
+      draw();
+      //no longer select the tile
+      selectedTile[i] = -1;
+    }
   }
 }
 
@@ -77,23 +84,18 @@ function draw() {
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');   
   ctx.font = "48px serif";
-  for (var f = 0; f < 10; f++)
-  {
-    //look at each tile  
+  for (var f = 0; f < board.tile.length; f++) {  
     for (var g = 0; g < board.tile.length; g++) {
       if (g != f) {
         if (board.tile[f].X != board.tile[g].X) {
-          if (board.tile[f].Y != board.tile[g].Y) {   
-difference = board.tile[f].X%60;            
-if (difference <= 30) {
-board.tile[f].X = board.tile[f].X - difference;
-} else {
-board.tile[f].X = board.tile[f].X + difference;
+          if (board.tile[f].Y != board.tile[g].Y) {
             ctx.clearRect(board.tile[f].oldX,board.tile[f].oldY,board.tile[f].Width,board.tile[f].Height);
-            ctx.fillStyle = "brown";
-            ctx.fillRect(board.tile[f].X,board.tile[f].Y,board.tile[f].Width,board.tile[f].Height);
-            ctx.fillStyle = "black";
-            ctx.fillText(board.tile[f].letter,board.tile[f].X+5,board.tile[f].Y+40);
+            for (var h = 0; h < board.tile.length; h++) {
+	      ctx.fillStyle = "brown";
+              ctx.fillRect(board.tile[h].X,board.tile[h].Y,board.tile[h].Width,board.tile[h].Height);
+              ctx.fillStyle = "black";
+              ctx.fillText(board.tile[h].letter,board.tile[h].X+5,board.tile[h].Y+40);
+            }
           }
         }
       }
@@ -108,7 +110,7 @@ function init() {
   var number = [];
   var hold;
   //Set variables for each tile
-  for (var i = 0; i < 10; i++){
+  for (var i = 0; i < 8; i++){
     number[i] = ((Math.random()*144));
     for (var g = 0; g <= i; g++)
     {
@@ -120,6 +122,6 @@ function init() {
       }
     }
   number[i] = hold;
-  board.tile[i] = {letter: alphabet.substring(number[i],number[i]+1), X:60*i, Y:60*i, Width:50, Height:50, distX:-1, distY: -1, oldX: 60*i, oldY:60*i};
+  board.tile[i] = {letter: alphabet.substring(number[i],number[i]+1), X:60*(i+1), Y:60*(i+1), Width:50, Height:50, distX:-1, distY: -1, oldX: 60*(i+1), oldY:60*(i+1)};
   }
 }
